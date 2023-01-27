03743fb1b53e9c3c05e233d3613b42f3cfdc5d3d
ROHEUNCHAN <nong0324@naver.com>
2023-01-27 16:54:57 +0900

    [스터디] 리액트를 다루는 기술 (P.133 - 3.3.6.1 is Required를 사용하여 필수 propTypes 설정)

---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const MyComponent = ({ name, children }) => {
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name}입니다.
+            <br />
+            children 값은 {children}
+            입니다.
+        </div>
+    );
+};
+
+MyComponent.defaultProps = {
+    name: "기본 이름",
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string // proptypes을 문자열만 들어갈 수 있도록 제어
+};
+
+export default MyComponent;
