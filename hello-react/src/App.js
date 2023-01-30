ab9e6c143b168590d761556cfc0c37e47fff969a
ROHEUNCHAN <nong0324@naver.com>
2023-01-30 17:17:22 +0900

    [스터디] 리액트를 다루는 기술 (P.133 - 3.4.2 함수 컴포넌트에서 useState 사용하기)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,9 @@
 // import React, {Component} from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
+// import MyComponent from './MyComponent';
 
 const App = () =>{
-  return <MyComponent name={"노은찬"}>리액트</MyComponent>;
+  return <Counter />;
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,22 +1,37 @@
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
-    return (
-        <div>
-            안녕하세요, 제 이름은 {name}입니다.
-            <br />
-            children 값은 {children}
-            입니다.
-        </div>
-    );
-};
+class MyComponent extends Component {
+    static defaultProps = {
+        name: "기본 이름",
+    };
+    static propTypes = {
+        name: PropTypes.string, // proptypes을 문자열만 들어갈 수 있도록 제어
+        favoriteNumber: PropTypes.number.isRequired,
+    };
 
-MyComponent.defaultProps = {
-    name: "기본 이름",
-};
+    render() {
+        const { name, favoriteNumber, children } = this.props;
+        return (
+            <div>
+                안녕하세요, 제 이름은 {name}입니다.
+                <br />
+                children 값은 {children}
+                입니다.
+                <br />
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        );
+    }
+}
 
-MyComponent.propTypes = {
-    name: PropTypes.string // proptypes을 문자열만 들어갈 수 있도록 제어
-};
+// MyComponent.defaultProps = {
+//     name: "기본 이름",
+// };
+
+// MyComponent.propTypes = {
+//     name: PropTypes.string, // proptypes을 문자열만 들어갈 수 있도록 제어
+//     favoriteNumber: PropTypes.number.isRequired,
+// };
 
 export default MyComponent;
