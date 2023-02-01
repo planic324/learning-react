212507d89f0e5703fbcd26e0a9ec525537e0ef53
ROHEUNCHAN <nong0324@naver.com>
2023-02-01 13:41:01 +0900

    [스터디] 리액트를 다루는 기술 (P.229 - 6.1 자바스크립트 배열 map() 함수)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,21 @@
-// import React, {Component} from 'react';
+import React, { Component } from "react";
 // import Counter from './Counter';
 // import MyComponent from './MyComponent';
 // import Say from './Say';
-import EventPractice from './EventPractice';
+import ScrollBox from "./ScrollBox";
 
-const App = () =>{
-  return <EventPractice />;
-};
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+                <button
+                  onClick={() => this.scrollBox.scrollToBottom()}>
+                  맨 밑으로
+                </button>
+            </div>
+        );
+    }
+}
 
 export default App;
--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,26 +1,77 @@
 import React, { Component } from "react";
 
 class EventPractice extends Component {
-    state ={
-        message: ''
-    }
-    render(){
+    state = {
+        userName: "",
+        message: "",
+    };
+
+    // **** constructor를 이용한 메서드 작성 방법 ****
+
+    // constructor(props) {
+    //     super(props);
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    // handleChange(e) {
+    //     this.setState({
+    //         message: e.target.value,
+    //     });
+    // }
+
+    // handleClick(e) {
+    //     alert(this.state.message);
+    //     this.setState({
+    //         message: "",
+    //     });
+    // }
+
+    handleChange = (e) => {
+        this.setState({
+            // message: e.target.value,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    handleClick = () => {
+        alert(this.state.username + ":" + this.state.message);
+        this.setState({
+            username: "",
+            message: "",
+        });
+    };
+
+    handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            this.handleClick();
+        }
+    };
+
+    render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
                 <input
                     type="text"
-                    name="message"
-                    placeholder="아무거나 입력해주세요."
-                    value={this.state.message}
-                    onChange={
-                        (e) => {
-                            console.log(e.target.value);
-                        }
-                    }
+                    name="username"
+                    placeholder="사용자명"
+                    value={this.state.username}
+                    onChange={this.handleChange}
                 />
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="내용을 입력하세요."
+                    value={this.state.message}
+                    onChange={this.handleChange}
+                    onKeyPress={this.handleKeyPress}
+                />
+                <button type="button" onClick={this.handleClick}>
+                    확인
+                </button>
             </div>
-        )
+        );
     }
 }
 
