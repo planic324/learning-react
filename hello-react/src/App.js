4552e8b884ba36fd5d0d482270052e7383522148
ROHEUNCHAN <nong0324@naver.com>
2023-02-06 17:03:40 +0900

    [스터디] 리액트를 다루는 기술 (P.280 - 7 Hooks)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,12 +3,34 @@ import React, { Component } from "react";
 // import MyComponent from './MyComponent';
 // import Say from './Say';
 // import ScrollBox from "./ScrollBox";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+// import ErrorBoundary from "./ErrorBoundary";
+
+// 랜덤색상을 생성
+function getRandomColor () {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+    state = {
+        color: '#000000'
+    }
+
+    handleClick = () =>{
+        this.setState({
+            color: getRandomColor()
+        });
+    }
     render() {
         return (
-            <IterationSample />
+            <div>
+                <button onClick={(this.handleClick)}>랜덤색상</button>
+                <LifeCycleSample color={this.state.color}/>
+                {/* <ErrorBoundary>
+                    
+                </ErrorBoundary> */}
+            </div>
+            
         );
     }
 }
