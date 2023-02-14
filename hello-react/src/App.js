021ac9529b59c8865e9f92856a7588660c0f0808
ROHEUNCHAN <nong0324@naver.com>
2023-02-14 17:00:45 +0900

    [스터디] 리액트를 다루는 기술 (P.386 - 10 일정 관리 웹 애플리케이션 만들기)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,25 +6,11 @@
 // import LifeCycleSample from "./LifeCycleSample";
 // import ErrorBoundary from "./ErrorBoundary";
 
-import React, { useState } from 'react';
-import Info from './Info';
+import Counter from './Counter';
 
 
 const App = () => {
-    const [visible, setVisible] = useState(false);
-    return (
-        <div>
-            <button
-            onClick={() => {
-                setVisible(!visible) 
-            }}
-            >
-                {visible ? '숨기기' : '보이기'}
-            </button>
-            <hr />
-            {visible && <Info />}
-        </div>
-    );
+    return <Counter />
 }
 
 
--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -9,8 +9,12 @@ class LifeCycleSample extends Component {
     myRef = null;
 
     constructor(props) {
-        super(props);
+        super(props); // react에서 construtor을 가져오는거다
         console.log("constructor");
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -21,15 +25,14 @@ class LifeCycleSample extends Component {
         return null;
     }
 
-    componentDidMount() {
-        console.log("componentDidMount");
-    }
-
+    // 다음 받을 Props값과 다음 Props를 가져온다.
+    // 특정 조건에 따라 업데이트를 막아줄 수 있는 함수 !
     shouldComponentUpdate(nextProps, nextState) {
         console.log("shouldComponentUpdate", nextProps, nextState);
         return nextState.number % 10 !== 4;
     }
-
+    
+    // 컴포넌트 제거
     componentWillUnmount() {
         console.log("componentWillUnmount");
     }
@@ -40,6 +43,7 @@ class LifeCycleSample extends Component {
         });
     };
 
+    // 업데이트 되기 바로 직전에 return해서 컴포넌트 파일의 value를 가져올 수 있다.
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log("getSnapshotBeforeUpdate");
         if (prevProps.color !== this.props.color) {
