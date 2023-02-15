fa23b0bbde2a18cde83dfa16bdcb3863bf87f5f0
ROHEUNCHAN <nong0324@naver.com>
2023-02-15 16:01:01 +0900

    [스터디] 리액트를 다루는 기술 (P.415 - 10.3 기능 구현하기)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,3 @@
-// import React, { Component } from "react";
-// import Counter from './Counter';
-// import MyComponent from './MyComponent';
-// import Say from './Say';
-// import ScrollBox from "./ScrollBox";
-// import LifeCycleSample from "./LifeCycleSample";
-// import ErrorBoundary from "./ErrorBoundary";
-
 import Counter from './Counter';
 
 
