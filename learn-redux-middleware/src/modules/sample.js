80babc695d0a31d31b5d4f007c2b7b4d3fb0aa84
ROHEUNCHAN <nong0324@naver.com>
2023-04-19 17:49:02 +0900

    fix

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -8,3 +8,21 @@ const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+export const getPost = id => async dispatch => {
+    dispatch ({ttype : GET_POST });
+    try {
+        const response = await api.getPost(id);
+        dispatch({
+            type : GET_POST_SUCCESS,
+            payload : response.data
+        }); // 요청성공
+    }   catch(e) {
+        dispatch({
+            type: GET_POST_FAILURE,
+            payload : e,
+            error: true
+        }); // 에러발생
+    }
+};
+
