d7727854f34665a1bbdcdc7b98bc000e0792b0c1
ROHEUNCHAN <nong0324@naver.com>
2023-05-11 17:51:28 +0900

    React redux study setting

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,16 +1,17 @@
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import rootReducer from './modules';
+import reportWebVitals from './reportWebVitals';
 
-const store = createStore(rootReducer, composeWithDevTools());
-
-ReactDOM.render(
-  <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
